d532a3c572ddcb6094e08cf17a1e5e4ebb880976
emanrashad173 <emanrashad173@gmail.com>
2023-09-29 17:03:14 +0300

    update api route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//posts and articles
 Route::apiResource('posts', 'PostController')->only(['index','store']);
 Route::apiResource('articles', 'ArticleController')->only(['index','store']);
 
